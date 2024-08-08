8f790353bbf2e38eea384fabba60d85ca3a30e70
Forthtilliath <forth@live.fr>
2024-08-08 17:54:26 +0200

    feat: add admin auth

--- src/@types/app.d.ts
+++ src/@types/app.d.ts
@@ -3,6 +3,14 @@ declare global {
 	type WithChildren<T = {}> = Readonly<T & { children?: React.ReactNode }>;
 
 	type Params<T> = Readonly<{ params: T }>;
+
+	namespace NodeJS {
+		interface ProcessEnv {
+			// NODE_ENV: "development" | "production" | "test";
+			ADMIN_USERNAME: string;
+			ADMIN_HASHED_PASSWORD: string;
+		}
+	}
 }
 
 export type {};
